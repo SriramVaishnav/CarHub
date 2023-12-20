2675c252c13c2333c6de6b36a238644e95050345
Sriram Vaishnav R <sriramvaishnavr@gmail.com>
2023-12-20 13:43:53 +0530

    final working version

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,19 @@
 // import { fetchCars } from "@utils";
 // import { HomeProps } from "@types";
 import { fuels, yearsOfProduction } from "@/constants";
-import { SearchBar, CustomFilter, Hero } from "@/components";
+import { SearchBar, CustomFilter, Hero, CarCard, ShowMore } from "@/components";
+import { fetchCars } from "@/utils";
 
-export default async function Home() {
-  // const allCars = await fetchCars({
-  //   manufacturer: searchParams.manufacturer || "",
-  //   year: searchParams.year || 2022,
-  //   fuel: searchParams.fuel || "",
-  //   limit: searchParams.limit || 10,
-  //   model: searchParams.model || "",
-  // });
-
-  // const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
+export default async function Home({ searchParams }) {
+  const allCars = await fetchCars({
+    manufacturer: searchParams.manufacturer ||  '',
+    year: searchParams.year ||  2021,
+    fuel: searchParams.fuel ||  '',
+    limit: searchParams.limit ||  10,
+    model: searchParams.model ||  '',
+  });
+  
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
 
   return (
     <main className='overflow-hidden'>
@@ -33,25 +34,22 @@ export default async function Home() {
           </div>
         </div>
 
-        {/* {!isDataEmpty ? (
+        {!isDataEmpty ? (
           <section>
-            <div className='home__cars-wrapper'>
+            <div className="home__cars-wrapper">
               {allCars?.map((car) => (
                 <CarCard car={car} />
               ))}
             </div>
-
-            <ShowMore
-              pageNumber={(searchParams.limit || 10) / 10}
-              isNext={(searchParams.limit || 10) > allCars.length}
-            />
+            <ShowMore pageNumber = {(searchParams.limit || 10)/10} isNext={(searchParams.limit || 10) > allCars.length} />
           </section>
         ) : (
-          <div className='home__error-container'>
-            <h2 className='text-black text-xl font-bold'>Oops, no results</h2>
+          <div className="home__error-container">
+            <h2 className="text-black text-xl font-bold">Oops, no results</h2>
             <p>{allCars?.message}</p>
           </div>
-        )} */}
+        )}
+
       </div>
     </main>
   );
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -35,7 +35,7 @@ const Footer = () => (
     </div>
 
     <div className='flex justify-between items-center flex-wrap mt-10 border-t border-gray-100 sm:px-16 px-6 py-10'>
-      <p>&copy; Developed By <a href="https://sriramvaishnav.com" className="hover:text-primary-blue">Sriram Vaishnav</a></p>
+      <p>&copy; Developed By <a href="https://sriramvaishnav.com" target="_blank" className="text-primary-blue hover:underline">Sriram Vaishnav</a></p>
 
       <div className="footer__copyrights-link">
         <Link href="/" className="text-gray-500">
